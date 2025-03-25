4d991dbd799caef6c2126f09111cb3f459b1795b
rifat-sarker <rifatswd@gmail.com>
2025-03-25 21:05:09 +0600

    add books and customers tables with sample data insertion

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,4 +1,6 @@
 -- Active: 1742810087227@@127.0.0.1@5432@bookstore_db
+
+-- create relation/table with type
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     title VARCHAR(150) NOT NULL,
@@ -8,6 +10,30 @@ CREATE TABLE books (
     published_year INT NOT NULL
 )
 
+-- get / see all data from books table
 select * from books;
 
+-- insert/add multiple data in the table
+INSERT INTO books ( title, author, price,stock, published_year) VALUES ('The Pragmatic Programmer','Andrew Hunt', 40.00, 10, 1999), ('Clean Code', 'Robert C. Martin', 35.00, 5, 2008), ('You Do not Know JS','Kyle Simpson', 30.00, 8, 2014), ('Refactoring', 'Martin Fowler', 50.00, 3, 1999), ('Database Design Principles', 'Jane Smith', 20.00, 0, 2018)
 
+INSERT INTO books VALUES (6,'Web Developer', 'Rifat Sarker', 50.00, 5, 2025);
+
+
+
+-- create customers relation/table
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    joined_date DATE DEFAULT CURRENT_DATE 
+)
+
+
+--  get all from customers relation
+select * from customers;
+
+
+-- insert multiple customers data
+INSERT INTO customers (name, email) VALUES ('Alice', 'alice@email.com'), ('Bob', 'bob@email.com'), ('Charlie', 'charlie@email.com') 
+
+DROP TABLE customers;
